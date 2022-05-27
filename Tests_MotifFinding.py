bf9dda60d63a85be3edbc2cf0b68fed5e038a98b
Roberto Costa Bullitta <46348284+rocobull@users.noreply.github.com>
2022-05-27 22:15:43 +0100

    Update and rename Motif_Consensus_Test.py to Tests_MotifFinding.py

--- Tests_MotifFinding.py
+++ Tests_MotifFinding.py
@@ -4,13 +4,13 @@ Created on Sun May 15 17:30:41 2022
 
 @author: vania
 """
-from Motif_Consensus import Motif_Consensus
+from MotifFinding import MotifFinding
 from MySeq import MySeq
 import unittest
 import random
 
 
-class Test_Motif_Consensus(unittest.TestCase):
+class Test_MotifFinding(unittest.TestCase):
     
     def setUp(self) -> None:
         seq1 = MySeq("ATAGAGCATAGA","dna")
@@ -18,10 +18,10 @@ class Test_Motif_Consensus(unittest.TestCase):
         seq3 = MySeq("AAGATAGGGG","dna")
         seq4 = MySeq("AAGACTTAGGGG","dna")
         
-        self.test = Motif_Consensus(4,[seq1,seq2,seq3,seq4])
-        self.test2 = Motif_Consensus(2, [seq1])
-        self.test3 = Motif_Consensus(5, [seq1, seq4])
-        self.test4 = Motif_Consensus()
+        self.test = MotifFinding(4,[seq1,seq2,seq3,seq4])
+        self.test2 = MotifFinding(2, [seq1])
+        self.test3 = MotifFinding(5, [seq1, seq4])
+        self.test4 = MotifFinding()
      
         
     def test_Heuristic(self):
